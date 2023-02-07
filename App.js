b046a681ddfd9558abce21341c6667497a4770bb
Sigadong <irwanto.regar30@gmail.com>
2023-02-07 22:22:18 +0700

    external style

--- App.js
+++ App.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-
 // Styling in react native
-// 2. Inline Style
+// 3. External Style
+import { styles } from './assets/style';
+
 export default function App() {
   return (
-    <View style={{
-      flex: 1,
-      padding: 10,
-      margin: 20,
-    }}>
-      <Text style={{
-        marginTop: 16,
-        paddingVertical: 8,
-        borderWidth: 3,
-        borderRadius: 8,
-        backgroundColor: 'lightgrey',
-        textAlign: 'center',
-        fontSize: 20,
-        fontWeight: 'bold',
-      }}>Hello World!</Text>
+    <View style={styles.container}>
+      <Text style={styles.teks}>Hello World!</Text>
     </View>
   )
 }
