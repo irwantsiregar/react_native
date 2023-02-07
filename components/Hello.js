248a3af16701ffc3337cb3ccc6d8cd88a1568223
Sigadong <irwanto.regar30@gmail.com>
2023-02-07 22:51:38 +0700

    functional component -> props destructuring

--- components/Hello.js
+++ components/Hello.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { styles } from '../assets/style';
 
-const HelloWorld = (props) => {
+const HelloWorld = ({ name }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.teks}>Hello {props.name},👋</Text>
+      <Text style={styles.teks}>Hello {name},👋</Text>
     </View>
   )
 }
