b1df47acf5190a3f5865ce1aad4d7ee9895ad190
Sigadong <irwanto.regar30@gmail.com>
2023-02-08 00:50:31 +0700

    styling with image

--- App.js
+++ App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { View, } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{
-      flex: 1,
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      alignItems: 'center',
-    }}>
-      <View style={{ height: 50, width: 50, backgroundColor: 'red' }} />
-      <View style={{ height: 50, width: 50, backgroundColor: 'grey' }} />
-      <View style={{ height: 50, width: 50, backgroundColor: 'blue' }} />
+    <View style={styles.container}>
+      <Image source={{ uri: 'https://images.unsplash.com/profile-1446404465118-3a53b909cc82' }} style={styles.gambar} />
+      <Text style={styles.teks}>Hello World!</Text>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  teks: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'darkblue',
+  },
+  gambar: {
+    margin: 10,
+    width: 100,
+    height: 100,
+    borderRadius: 30 / 2,
+  }
+})
