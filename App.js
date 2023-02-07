26a6c95a60022bc624c6e10a4f693a0f914175fc
Sigadong <irwanto.regar30@gmail.com>
2023-02-07 22:15:47 +0700

    inline style

--- App.js
+++ App.js
@@ -1,31 +1,26 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+// Styling in react native
+// 2. Inline Style
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.teks}>Hello World!</Text>
+    <View style={{
+      flex: 1,
+      padding: 10,
+      margin: 20,
+    }}>
+      <Text style={{
+        marginTop: 16,
+        paddingVertical: 8,
+        borderWidth: 3,
+        borderRadius: 8,
+        backgroundColor: 'lightgrey',
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: 'bold',
+      }}>Hello World!</Text>
     </View>
   )
 }
 
-// Styling in react native
-
-// 1. Internal Style
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-    margin: 20,
-  },
-  teks: {
-    marginTop: 16,
-    paddingVertical: 8,
-    borderWidth: 3,
-    borderRadius: 8,
-    backgroundColor: 'lightgrey',
-    textAlign: 'center',
-    fontSize: 20,
-    fontWeight: 'bold',
-  }
-});
