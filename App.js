78c12ef267889352dc13d88e39ef1cb7184a4cee
Sigadong <irwanto.regar30@gmail.com>
2023-02-08 00:21:30 +0700

    flexbox: flexDirection

--- App.js
+++ App.js
@@ -3,10 +3,15 @@ import { View, } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: 'red' }} />
-      <View style={{ flex: 2, backgroundColor: 'grey' }} />
-      <View style={{ flex: 3, backgroundColor: 'blue' }} />
+    <View style={{
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+    }}>
+      <View style={{ height: 50, width: 50, backgroundColor: 'red' }} />
+      <View style={{ height: 50, width: 50, backgroundColor: 'grey' }} />
+      <View style={{ height: 50, width: 50, backgroundColor: 'blue' }} />
     </View>
   )
 }
