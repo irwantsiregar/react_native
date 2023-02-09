49337588212447408cfc5801a19b311caf47a349
Sigadong <irwanto.regar30@gmail.com>
2023-02-09 17:34:13 +0700

    added splash screen to application

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View, Image, } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './components/Home';
@@ -7,9 +8,28 @@ import Detail from './components/Detail';
 const Stack = createStackNavigator();
 
 export default function App() {
+  const splashScreen = ({ navigation }) => {
+    setTimeout(() => {
+      navigation.replace('Home');
+    }, 5000);
+
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Image source={require('./assets/logo.png')} style={{ width: 100, height: 100 }} />
+      </View>
+    )
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen
+          name="Splash"
+          component={splashScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
         <Stack.Screen
           name="Home"
           component={Home}
